470ebbdd88577db631ca1987db1ae40b40cd0f03
Guzz-T <126437616+Guzz-T@users.noreply.github.com>
2025-03-06 08:53:30 +0100

    Improvements within dump_changes script

--- luxtronik/scripts/dump_changes.py
+++ luxtronik/scripts/dump_changes.py
@@ -4,9 +4,11 @@
 
 """Script to dump all value changes from Luxtronik controller"""
 
-import os
+import sys
 import time
 import argparse
+import select
+from collections import OrderedDict
 
 from luxtronik import LuxtronikSocketInterface
 from luxtronik.constants import LUXTRONIK_DEFAULT_PORT
@@ -31,6 +33,8 @@ def dump_changes():
     # pylint: enable=duplicate-code
     changes = {}
 
+    print("\033[2J") # clear screen
+
     while True:
         # Get new data
         this_data = client.read()
@@ -38,7 +42,7 @@ def dump_changes():
         # Compare this values with the initial values
         # and add changes to dictionary
         for number, param in this_data.parameters:
-            key = f"para_{number}"
+            key = f"para_{str(number).zfill(5)}"
             prev_param = prev_data.parameters.get(number)
             if param.raw != prev_param.raw:
                 changes[key] = (
@@ -46,11 +50,11 @@ def dump_changes():
                 )
             elif key in changes:
                 changes[key] = (
-                    f"para: Number: {number:<5} Name: {prev_param.name:<60} " + f"Value: {prev_param} -> reverted"
+                    f"para: Number: {number:<5} Name: {prev_param.name:<60} " + f"Value: {prev_param}"
                 )
 
         for number, calc in this_data.calculations:
-            key = f"calc_{number}"
+            key = f"calc_{str(number).zfill(5)}"
             prev_calc = prev_data.calculations.get(number)
             if calc.raw != prev_calc.raw:
                 changes[key] = (
@@ -58,11 +62,11 @@ def dump_changes():
                 )
             elif key in changes:
                 changes[key] = (
-                    f"calc: Number: {number:<5} Name: {prev_calc.name:<60} " + f"Value: {prev_calc} -> reverted"
+                    f"calc: Number: {number:<5} Name: {prev_calc.name:<60} " + f"Value: {prev_calc}"
                 )
 
         for number, visi in this_data.visibilities:
-            key = f"visi_{number}"
+            key = f"visi_{str(number).zfill(5)}"
             prev_visi = prev_data.visibilities.get(number)
             if visi.raw != prev_visi.raw:
                 changes[key] = (
@@ -70,15 +74,31 @@ def dump_changes():
                 )
             elif key in changes:
                 changes[key] = (
-                    f"visi: Number: {number:<5} Name: {prev_visi.name:<60} " + f"Value: {prev_visi} -> reverted"
+                    f"visi: Number: {number:<5} Name: {prev_visi.name:<60} " + f"Value: {prev_visi}"
                 )
 
         # Print changes
-        os.system("clear")
+        print("\033[H") # Go-to home, line 0
         print("=" * 80)
-        for key, values in changes.items():
-            print(values)
+        print("Press a key and enter to: q = quit; r = reset")
+        print("=" * 80)
+        sorted_changes = OrderedDict(sorted(changes.items()))
+        for key, values in sorted_changes.items():
+            print(values + "\033[0K") # clear residual line
+        print("\n")
+
+        # Read stdin
+        input, _, _ = select.select([sys.stdin], [], [], 0.1)
+        if input:
+            key = sys.stdin.read(1)
+            if key == 'q':
+                break
+            elif key == 'r':
+                prev_data = client.read()
+                changes = {}
+                print("\033[2J") # clear screen
         time.sleep(1)
+
 
 
 if __name__ == "__main__":
