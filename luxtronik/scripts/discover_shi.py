c445be6d5016b0a1949d0d5fedc06bd1fb994dca
Guzz-T <Guzz-T@users.noreply.github.com>
2025-11-04 20:48:37 +0100

    wip

--- luxtronik/scripts/discover_shi.py
+++ luxtronik/scripts/discover_shi.py
@@ -17,9 +17,9 @@ from luxtronik.shi.holdings import HOLDINGS_DEFINITIONS
 
 logging.disable(logging.CRITICAL)
 
-def get_undefined_fields(definitions, start, count):
+def get_undefined(definitions, start, count):
     skip_count = 0
-    undefined_fields = {}
+    undefined = []
     for i in range(start, start + count):
         # Skip addresses that belongs to a previous field
         if skip_count > 0:
@@ -28,25 +28,42 @@ def get_undefined_fields(definitions, start, count):
         definition = definitions[i]
         # Add unknown
         if definition is None:
-            undefined_fields[i] = Unknown(f"unknown_{definitions.name}_{i}", False)
-            #print(f"Add unknown {i}")
+            undefined.append((i, None, Unknown(f"unknown_{definitions.name}_{i}", False)))
         else:
             skip_count = definition.count - 1
-            #print(f"Skip {definition}")
-    return undefined_fields
+    return undefined
 
-def dump_fields(undefined_fields, offset, read_cb):
-    for number, field in undefined_fields.items():
+def get_defined(definitions):
+    defined = []
+    for definition in definitions:
+        defined.append((definition.index, definition, definition.create_field()))
+    return defined
+
+def dump_undefined(undefined, offset, read_cb):
+    for number, _, field in undefined:
         print(f"Number: {number:<5}", end="\r")
         data = read_cb(number + offset, 1)
         if data is not None:
             field.raw = data[0]
             print_dump_row(number, field)
 
+def dump_defined(defined, offset, read_cb):
+    for number, definition, field in defined:
+        print(f"Number: {number:<5}", end="\r")
+        data = read_cb(number + offset, definition.count)
+        if data is None:
+            field.raw = None
+            print_dump_row(number, field)
+
 def discover_fields(definitions, start, count, read_cb):
     print_dump_header(f"Undefined but existing {definitions.name}s")
-    undefined_fields = get_undefined_fields(definitions, start, count)
-    dump_fields(undefined_fields, definitions.offset, read_cb)
+    undefined = get_undefined(definitions, start, count)
+    dump_undefined(undefined, definitions.offset, read_cb)
+
+def discontinue_fields(definitions, read_cb):
+    print_dump_header(f"Defined but not existing {definitions.name}s")
+    defined = get_defined(definitions)
+    dump_defined(defined, definitions.offset, read_cb)
 
 def discover_shi():
     parser = create_default_args_parser(
@@ -66,7 +83,12 @@ def discover_shi():
     client = LuxtronikModbusTcpInterface(args.ip, args.port)
 
     discover_fields(INPUTS_DEFINITIONS, 0, args.count, client.read_inputs)
+    discontinue_fields(INPUTS_DEFINITIONS, client.read_inputs)
     discover_fields(HOLDINGS_DEFINITIONS, 0, args.count, client.read_holdings)
+    discontinue_fields(HOLDINGS_DEFINITIONS, client.read_holdings)
+
+    # Clear last line if nothing was found
+    print(' '*100)
 
 
 if __name__ == "__main__":
